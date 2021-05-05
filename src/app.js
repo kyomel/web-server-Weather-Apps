824008ef381e078ddc9401c6c0260c783ea1c236
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-05-05 16:16:08 +0700

    [kyomel] add template engine handbars

--- src/app.js
+++ src/app.js
@@ -1,18 +1,50 @@
 const path = require('path');
 const express = require('express');
-
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Michael Stevan'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Michael Stevan'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Michael Stevan'
+    })
+});
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is raining',
         location: 'Yogyakarta' 
     });
-})
+});
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000!')
